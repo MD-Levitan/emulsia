9a809a8705a24f828c6a25f2710ce15e37f948a7
MD-Levitan <ovsyanka@protonmail.com>
2020-11-10 23:04:21 +0300

    Add more info about functionlaity

--- emulsia/emulator.py
+++ emulsia/emulator.py
@@ -11,7 +11,8 @@ from .call_viewer import CallViewer
 
 
 class EmulatorConfig:
-    def __init__(self, hook_code: bool, hook_inter: bool, hook_mem: bool, hook_mem_invalid: bool, hook_fetch):
+    def __init__(self, hook_code: bool, hook_inter: bool, hook_mem: bool,
+                 hook_mem_invalid: bool, hook_fetch):
         self._hook_code = hook_code
         self._hook_inter = hook_inter
         self._hook_mem = hook_mem
@@ -19,11 +20,9 @@ class EmulatorConfig:
         self._hook_fetch = hook_fetch
 
     def __str__(self):
-        return "Hooks configuaration: code - {}, interupt - {}, memory - {}, invalid memory - {}, fetch - {}".format(self._hook_code,
-                                                                                                                     self._hook_inter,
-                                                                                                                     self._hook_mem,
-                                                                                                                     self._hook_mem_invalid,
-                                                                                                                     self._hook_fetch)
+        return "Hooks configuaration: code - {}, interupt - {}, memory - {}, invalid memory - {}, fetch - {}".format(
+            self._hook_code, self._hook_inter, self._hook_mem,
+            self._hook_mem_invalid, self._hook_fetch)
 
     @property
     def hook_code(self):
@@ -55,38 +54,41 @@ class Emulator:
     def __init_hooks__(self):
         def hook_code(uc: Uc, address, size, user_data):
             print()
-            cprint(">>> Tracing instruction at 0x{:x}, instruction size = 0x{:x}".format(
-                address, size), 'green')
-            print("Registers: r0 - {}, r1 - {}, r2 - {}, r3 - {}, r4 - {}, r5 - {}, r6 - {}, r7 - {}, r8 - {} sb - {}, pc - {}, lr - {}, sp - {}".format(
-                hex(uc.reg_read(UC_ARM_REG_R0)),
-                hex(uc.reg_read(UC_ARM_REG_R1)),
-                hex(uc.reg_read(UC_ARM_REG_R2)),
-                hex(uc.reg_read(UC_ARM_REG_R3)),
-                hex(uc.reg_read(UC_ARM_REG_R4)),
-                hex(uc.reg_read(UC_ARM_REG_R5)),
-                hex(uc.reg_read(UC_ARM_REG_R6)),
-                hex(uc.reg_read(UC_ARM_REG_R7)),
-                hex(uc.reg_read(UC_ARM_REG_R8)),
-
-                hex(uc.reg_read(UC_ARM_REG_SB)),
-                hex(uc.reg_read(UC_ARM_REG_PC)),
-                hex(uc.reg_read(UC_ARM_REG_LR)),
-                hex(uc.reg_read(UC_ARM_REG_SP))))
+            cprint(
+                ">>> Tracing instruction at 0x{:x}, instruction size = 0x{:x}".
+                format(address, size), 'green')
+            print(
+                "Registers: r0 - {}, r1 - {}, r2 - {}, r3 - {}, r4 - {}, r5 - {}, r6 - {}, r7 - {}, r8 - {} sb - {}, pc - {}, lr - {}, sp - {}"
+                .format(hex(uc.reg_read(UC_ARM_REG_R0)),
+                        hex(uc.reg_read(UC_ARM_REG_R1)),
+                        hex(uc.reg_read(UC_ARM_REG_R2)),
+                        hex(uc.reg_read(UC_ARM_REG_R3)),
+                        hex(uc.reg_read(UC_ARM_REG_R4)),
+                        hex(uc.reg_read(UC_ARM_REG_R5)),
+                        hex(uc.reg_read(UC_ARM_REG_R6)),
+                        hex(uc.reg_read(UC_ARM_REG_R7)),
+                        hex(uc.reg_read(UC_ARM_REG_R8)),
+                        hex(uc.reg_read(UC_ARM_REG_SB)),
+                        hex(uc.reg_read(UC_ARM_REG_PC)),
+                        hex(uc.reg_read(UC_ARM_REG_LR)),
+                        hex(uc.reg_read(UC_ARM_REG_SP))))
 
             if self.arch == UC_ARCH_ARM:
-                disasm_thumb = list(self.md_thumb.disasm(
-                    uc.mem_read(address, size), address))
-                disasm_arm = list(self.md_arm.disasm(
-                    uc.mem_read(address, size), address))
+                disasm_thumb = list(
+                    self.md_thumb.disasm(uc.mem_read(address, size), address))
+                disasm_arm = list(
+                    self.md_arm.disasm(uc.mem_read(address, size), address))
 
-                if len(disasm_thumb) == 2 and len(disasm_arm) == 1 and self.arm_mode == True:
+                if len(disasm_thumb) == 2 and len(
+                        disasm_arm) == 1 and self.arm_mode == True:
                     self.arm_mode = False
 
-                if len(disasm_thumb) == 1 and len(disasm_arm) != 1 and self.arm_mode == False:
+                if len(disasm_thumb) == 1 and len(
+                        disasm_arm) != 1 and self.arm_mode == False:
                     self.arm_mode = True
 
-                print("Arm mode: {}".format(
-                    "THUMB2" if self.arm_mode == True else "ARM"))
+                print("Arm mode: {}".format("THUMB2" if self.arm_mode ==
+                                            True else "ARM"))
                 for i in disasm_thumb if self.arm_mode == 1 else disasm_arm:
                     print("0x%x:\t%s\t%s" % (i.address, i.mnemonic, i.op_str))
                     self._call_view.add_call(i.op_str, i.address, i)
@@ -95,32 +97,37 @@ class Emulator:
                 for i in self.md.disasm(uc.mem_read(address, size), address):
                     print("0x%x:\t%s\t%s" % (i.address, i.mnemonic, i.op_str))
 
+            if address in self.hook_functions:
+                self.hook_functions[address](uc)
+
         def hook_intr(uc, intno, user_data):
             if intno != 0x80:
                 print("got interrupt %x ???" % intno)
-                uc.emu_stop()
-                return
+                # uc.emu_stop()
+                # return
 
         def hook_mem_invalid(uc, access, address, size, value, user_data):
             if access == UC_MEM_WRITE_UNMAPPED:
-                print(">>> Missing memory is being WRITE at 0x%x, data size = %u, data value = 0x%x" % (
-                    address, size, value))
+                print(
+                    ">>> Missing memory is being WRITE at 0x%x, data size = %u, data value = 0x%x"
+                    % (address, size, value))
                 return False
             else:
-                print(">>> Missing memory is being READ at 0x%x, data size = %u, data value = 0x%x" % (
-                    address, size, value))
+                print(
+                    ">>> Missing memory is being READ at 0x%x, data size = %u, data value = 0x%x"
+                    % (address, size, value))
                 return False
 
         def hook_mem(uc, access, address, size, value, user_data):
-            self._mem_view.access_memory(address, size, MemoryAccess.READ if access == UC_MEM_READ else MemoryAccess.WRITE,
-                                         uc.reg_read(UC_ARM_REG_PC),
-                                         value if access == UC_MEM_WRITE else int.from_bytes(
-                                             uc.mem_read(address, size), byteorder='little')
-                                         )
+            self._mem_view.access_memory(
+                address, size, MemoryAccess.READ
+                if access == UC_MEM_READ else MemoryAccess.WRITE,
+                uc.reg_read(UC_ARM_REG_PC),
+                value if access == UC_MEM_WRITE else int.from_bytes(
+                    uc.mem_read(address, size), byteorder='little'))
 
         def hook_fetch(uc, access, address, size, value, user_data):
-            print("UC_MEM_FETCH of 0x%x, data size = %u" %
-                  (address, size))
+            print("UC_MEM_FETCH of 0x%x, data size = %u" % (address, size))
 
         if self.config.hook_code == True:
             self.mu.hook_add(UC_HOOK_CODE, hook_code)
@@ -132,16 +139,23 @@ class Emulator:
             self.mu.hook_add(UC_HOOK_MEM_READ | UC_HOOK_MEM_WRITE, hook_mem)
 
         if self.config.hook_mem_invalid == True:
-            self.mu.hook_add(UC_HOOK_MEM_READ_UNMAPPED |
-                             UC_HOOK_MEM_WRITE_UNMAPPED, hook_mem_invalid)
+            self.mu.hook_add(
+                UC_HOOK_MEM_READ_UNMAPPED | UC_HOOK_MEM_WRITE_UNMAPPED,
+                hook_mem_invalid)
 
         if self.config.hook_fetch == True:
-            self.mu.hook_add(UC_HOOK_MEM_FETCH | UC_HOOK_MEM_FETCH_INVALID |
-                             UC_HOOK_MEM_FETCH_UNMAPPED | UC_HOOK_MEM_FETCH_PROT |
-                             UC_HOOK_MEM_PROT | UC_HOOK_MEM_PROT, hook_fetch)
+            self.mu.hook_add(
+                UC_HOOK_MEM_FETCH | UC_HOOK_MEM_FETCH_INVALID
+                | UC_HOOK_MEM_FETCH_UNMAPPED | UC_HOOK_MEM_FETCH_PROT
+                | UC_HOOK_MEM_PROT | UC_HOOK_MEM_PROT, hook_fetch)
 
-    def __init__(self, config: EmulatorConfig = VerboseEmulatorConfig, arch=UC_ARCH_ARM, mode=UC_MODE_THUMB,
-                 arch_md=CS_ARCH_ARM, mode_md=CS_MODE_THUMB, base_address=0x0000):
+    def __init__(self,
+                 config: EmulatorConfig = VerboseEmulatorConfig,
+                 arch=UC_ARCH_ARM,
+                 mode=UC_MODE_THUMB,
+                 arch_md=CS_ARCH_ARM,
+                 mode_md=CS_MODE_THUMB,
+                 base_address=0x0000):
 
         self.arch = arch
         self.mode = mode
@@ -165,24 +179,33 @@ class Emulator:
 
         self._mem_view = MemoryViewer()
         self._call_view = CallViewer()
+        self.hook_functions = {} 
 
         self.config = config
         self.base_address = base_address
 
-        self.mu.mem_map(base_address, 4000 * 1024 * 1024)
-        self._mem_view.map_memory(base_address, 4000 * 1024 * 1024)
+        self.mem_range = 4000 * 1024 * 1024
+        self.bin_range = 4000 * 1024
 
-        self.__init_hooks__()
+        self.mu.mem_map(base_address, self.mem_range)
+        self._mem_view.map_memory(base_address, self.mem_range)
 
     def init_data(self, pointer: int, data: bytes):
         self.mu.mem_write(self.base_address + pointer, data)
-        self._mem_view.init_memory(pointer, len(
-            data), MemoryType.TEXT_MEMORY, data)
+        self._mem_view.init_memory(pointer, len(data), MemoryType.TEXT_MEMORY,
+                                   data)
 
-    def init_data_file(self, filename: str, start: int, size: int, address=None):
+    def init_data_file(self,
+                       filename: str,
+                       start: int,
+                       size: int,
+                       address=None):
         address = address if address is not None else start
-        self.init_data(address, open(filename, 'br').read()
-                       [start: start + size])
+        self.init_data(address,
+                       open(filename, 'br').read()[start:start + size])
+
+    def init_binary(self, filename: str):
+        self.init_data(0, open(filename, 'br').read())
 
     def init_reg(self, reg_type: int, reg_value):
         self.mu.reg_write(reg_type, reg_value)
@@ -198,30 +221,72 @@ class Emulator:
             else:
                 pass
 
-    def prepare_stack(self, stack_address: int, stack_data: bytes, stack_size: int = 0x100):
+    def prepare_stack(self,
+                      stack_address: int,
+                      stack_size_top: int = 0x300,
+                      stack_size_bottom: int = 0x300,
+                      stack_data: bytes = None):
+        """! Prepare stack of emulator. As it emulate function stack has to have free memory at top and bottom.
+
+        @param stack_address        adrress of current stack
+        @param stack_size_top       size of free memory at top of stack
+        @param stack_size_bottom    size of free memory at bottom of stack
+        @param stack_data           value of stack
+        """
         self.stack_address = stack_address
+        if stack_data is None:
+            stack_data = b'\xff' * (stack_size_top + stack_size_bottom)
+        if len(stack_data) < stack_size_top + stack_size_bottom:
+            stack_data.append(
+                b'\xff' *
+                (stack_size_top + stack_size_bottom - len(stack_data)))
 
-        self.mu.mem_write(self.base_address + stack_address, stack_data)
-
-        # Fill empty stack
-        self.mu.mem_write(self.base_address +
-                          stack_address - stack_size, b'\xff' * stack_size)
-
-        self._mem_view.init_memory(self.base_address + stack_address - stack_size,
-                                   stack_size, MemoryType.STACK_MEMORY, b'\xff' * stack_size)
-
+        self.mu.mem_write(self.base_address + stack_address - stack_size_top,
+                          stack_data)
+        self._mem_view.init_memory(
+            self.base_address + stack_address - stack_size_top,
+            len(stack_data), MemoryType.STACK_MEMORY, stack_data)
         self.mu.reg_write(UC_ARM_REG_SP, stack_address)
 
     def emulate(self, start: int, end: int, count: int):
         assert start <= end
-        self.mu.emu_start(self.base_address + start + (0x01 if self.mode ==
-                                                       UC_MODE_THUMB else 0x00), end, count=count)
 
-    def read_stack(self, size=0x100):
+        self.__init_hooks__()
+        self.mu.emu_start(self.base_address + start +
+                          (0x01 if self.mode == UC_MODE_THUMB else 0x00),
+                          end,
+                          count=count)
+
+    def create_binary(self, start: int, end: int, count: int, file: str):
+        assert start <= end
+        output = open(file, "wb")
+        output.write(b'\x00\x00\x00\x00' * (self.bin_range // 0x04))
+
+        def hook_code_writeable(uc: Uc, address, size, user_data):
+            data = uc.mem_read(address, size)
+            output.seek(address)
+            output.write(data)
+
+        self.mu.hook_add(UC_HOOK_CODE, hook_code_writeable)
+
+        self.mu.emu_start(self.base_address + start +
+                          (0x01 if self.mode == UC_MODE_THUMB else 0x00),
+                          end,
+                          count=count)
+
+    def read_stack(self, size=0x300):
         return self.read_memory(self.stack_address, size)
 
     def read_memory(self, address: int, size=0x100):
         return self.mu.mem_read(self.base_address + address, size)
+
+    def add_function_hook(self, address: int, func):
+        """! Add function hook by address.
+        
+        @param address      address of fucntion
+        @param func         function that will emulate function using unicorn
+        """
+        self.hook_functions[address] = func
 
     @property
     def mem_view(self):
--- emulsia/memory_viewer.py
+++ emulsia/memory_viewer.py
@@ -2,6 +2,7 @@ from unicorn import *
 from enum import Enum
 from termcolor import colored, cprint
 
+
 class MemoryType(Enum):
     STACK_MEMORY = 0
     HEAP_MEMORY = 1
@@ -19,53 +20,109 @@ class MemoryAccess(Enum):
 
 
 class MemoryViewer:
-
     def __init__(self):
 
         self.memory_access_map = {}
         self.memory_map = {}
+        self.invalid_access = {}
 
         self.start_addr = 0
         self.end_addr = 0
 
     def map_memory(self, start_address: int, size: int):
+        """! Map memory(just define memory space suitable for binary file).
+
+        @param start_address    base address of mapped memory
+        @param size             size of memory space
+        """
         self.start_addr = start_address
         self.end_addr = start_address + size
 
-    def init_memory(self, address: int, size: int, init_type: MemoryType, value: bytearray):
+    def init_memory(self, address: int, size: int, init_type: MemoryType,
+                    value: bytearray):
+        """! Initialized a part of memory.
+        
+        @param address          address of memory
+        @param size             size of memory
+        @param init_type        type of memory: STACK, HEAP, TEXT
+        @param value            value of memory
+        """
+
         for size_iter in range(0, size):
-            self.memory_map[address + size_iter] = (value[size_iter], init_type)
+            self.memory_map[address + size_iter] = (value[size_iter],
+                                                    init_type)
 
     def check_memory(self, address: int, size: int) -> list:
+        """! Check memory in map if it was initialized.
+        
+        @param address          address of memory
+        @param size             size of memory
+
+        @return list            list of MemoryTypes. Use list for situation when we try to access to different types of memoty
+        """
         type_list = []
         for size_iter in range(0, size):
             mem_addr = address + size_iter
-            type_list.append(self.memory_map.get(mem_addr, MemoryType.UNDEFINED_MEMORY if self.start_addr <=
-                                                 mem_addr <= self.end_addr else MemoryType.UNMAPPED_MEMORY))
+            type_list.append(
+                self.memory_map.get(
+                    mem_addr, MemoryType.UNDEFINED_MEMORY
+                    if self.start_addr <= mem_addr <= self.end_addr else
+                    MemoryType.UNMAPPED_MEMORY))
 
         return type_list
 
-    def access_memory(self, address: int, size: int, access_type: MemoryAccess, pointer: int, value):
-        """ Add info about access to memory.
+    def access_memory(self, address: int, size: int, access_type: MemoryAccess,
+                      pointer: int, value):
+        """! Add access to memory.
 
+        @param address      address of memory
+        @param size         size of memory
+        @param access_type  type of accesing: Read, Write
+        @param pointer      address from where access to memory
+        @param value        value on address
         """
         types = self.check_memory(address, size)
         for i in range(0, len(types)):
             type_ = types[i]
             if type_ == MemoryType.UNDEFINED_MEMORY or type_ == MemoryType.UNMAPPED_MEMORY:
-                # TODO: add execption
-                cprint("Trying access to INVALID memory {:08x}".format(address + i), "red")
+                cprint(
+                    "Trying access to INVALID memory {:08x}".format(address +
+                                                                    i), "red")
+
+                if address not in self.invalid_access:
+                    self.invalid_access[address] = [(size, pointer,
+                                                     access_type, value)]
+                else:
+                    self.invalid_access[address].append(
+                        (size, pointer, access_type, value))
 
         values = self.memory_access_map.get(address, None)
         if values is not None:
             values.append((size, pointer, access_type, value))
         else:
-            self.memory_access_map[address] = [
-                (size, pointer, access_type, value)]
+            self.memory_access_map[address] = [(size, pointer, access_type,
+                                                value)]
 
     def print_access(self):
+        """! Print inforamtion about all access tries."""
+        print()
+        print("===              Access Map              ===")
+        print("Address:   Size:  Pointer:   Access: Value:")
+
         for address, info_list in sorted(self.memory_access_map.items()):
             for info in info_list:
-                print("{:08x} - size: {:02x}, pointer: {:08x}, access: {}, value: {:08x}".format(address,
-                                                                                                 info[0], info[1], "R" if info[2] == MemoryAccess.READ else "W",
-                                                                                                 info[3]))
+                print("{:08x} | {:02x}   | {:08x} | {}      | {:08x}".format(
+                    address, info[0], info[1],
+                    "R" if info[2] == MemoryAccess.READ else "W", info[3]))
+
+    def print_invlaid_memory(self):
+        """! Print access tries to invalid memory."""
+        print()
+        print("===            Invalid Memory            ===")
+        print("Address:   Size:  Pointer:   Access: Value:")
+
+        for address, info_list in sorted(self.invalid_access.items()):
+            for info in info_list:
+                print("{:08x} | {:02x}   | {:08x} | {}      | {:08x}".format(
+                    address, info[0], info[1],
+                    "R" if info[2] == MemoryAccess.READ else "W", info[3]))
